e3793e5eb9e261249c7d3f8411d393c2db6c2a88
joanna <joanna.ewa.zawalich@gmail.com>
2021-09-19 21:17:17 +0200

    - corrected update method - added comments

--- src/main/java/com/joanna/springBoot1/api/BookController.java
+++ src/main/java/com/joanna/springBoot1/api/BookController.java
@@ -17,27 +17,37 @@ public class BookController {
         this.bookService = personService;
     }
 
+    //e.g. POST -> raw JSON file
+    //    {
+    //        "index": 333,
+    //        "author": "Michaił Bułhakow",
+    //        "title": "Mistrz i Małgorzata"
+    //    }
     @PostMapping
     public void addBook(@RequestBody Book book) {
         bookService.addBook(book);
     }
 
+    //http://localhost:8080/books
     @GetMapping
     public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
 
+    //e.g. http://localhost:8080/books/333, where '333' is the index of a book to GET info
     @GetMapping(path = "{index}")
     public Book getBookByIndex(@PathVariable("index") int index) {
         return bookService.getBookByIndex(index)
                 .orElse(null); //e.g. 404 or msg or ...
     }
 
+    //e.g. http://localhost:8080/books/123, where '123' is the index of a book to DELETE
     @DeleteMapping(path = "{index}")
     public void deleteBookByIndex(@PathVariable("index") int index) {
         bookService.deleteBookByIndex(index);
     }
 
+    //e.g. http://localhost:8080/books/234, where '234' is the index of a book to update (PUT)
     @PutMapping(path = "{index}")
     public void updateBookByIndex(@PathVariable("index") int index, @RequestBody Book book) {
         bookService.updateBookByIndex(index,book);
--- src/main/java/com/joanna/springBoot1/dao/FakeBookDAO.java
+++ src/main/java/com/joanna/springBoot1/dao/FakeBookDAO.java
@@ -14,7 +14,7 @@ public class FakeBookDAO implements BookDAO {
     static {
         {
             books.add(new Book(123,"Stephen King", "The Dark Tower"));
-            books.add(new Book(234, "Maria Miduch", "Oddech Boga"));
+            books.add(new Book(234, "George Orwell", "1984"));
         }
     }
     @Override
@@ -32,20 +32,40 @@ public class FakeBookDAO implements BookDAO {
     public Optional<Book> selectBookByIndex(int index) {
         return books.stream()
                 //.filter(book -> book.getIndex().equals(index))
+                .filter(book -> book.getIndex()==index)
                 .findFirst();
+        //'easier' way: - before Java8/11
+        //books.get(index);
+        //void (no return)
     }
 
     @Override
     public int deleteBookByIndex(int index) {
         Optional<Book> book = selectBookByIndex(index);
-        if (book.isEmpty()) return 0;
-        books.remove(book.get());
+        if (book.isEmpty()) return 0; //Optional.isEmpty() - If a value is not present, returns true, otherwise false
+        books.remove(book.get()); //Optional.get() - If a value is present in this Optional, returns the value (otherwise throws NoSuchElementException)
         return 1;
+        //'easier' way:
+        //books.remove(index);
+        //void (no return)
     }
 
     @Override
-    public int updateBookByIndex(int index, Book book) {
-        return 1;//selectBookByIndex(index);
+    public int updateBookByIndex(int index, Book updatedBook) {
+        return selectBookByIndex(index)
+                .map(book -> { //zakładamy, że w Optional'u jest książka
+                    int indexOfBook = books.indexOf(book); //wyszukanie indeksu ksiazki na liscie (w DB)
+                    if (indexOfBook >= 0) { //jesli indeks istnieje...
+                        books.set(indexOfBook, new Book(index,updatedBook.getAuthor(),updatedBook.getTitle())); //ustawienie nowych wartosci pod tym indeksem
+                        return 1;
+                    }
+                    return 0; //jesli indeks nie istnieje...
+                })
+                .orElse(0); //Optional.orElse - If a value is present, returns the value, otherwise returns other (default value); here: other=0
     }
-
+    //'easier' way:
+    //Book book = books.get(updatedBook.getIndex());
+    //book.setTitle(updatedBook.getTitle());
+    //...
+    //books.add(...);
 }
